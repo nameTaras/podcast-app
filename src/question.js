a3d0385e242372a0e5558ef983bd69b6868b9c47
Taras <shchurtaras@hotmail.com>
2021-04-04 10:45:54 +0300

    display questions

--- src/question.js
+++ src/question.js
@@ -8,6 +8,44 @@ export class Question {
             }
         })
             .then(response => response.json())
-            .then(response => console.log(response))
+            .then(response => {
+                question.id = response.name
+                return question
+            })
+            .then(addToLocalStorage)
+            .then(Question.renderList)
     }
+
+    static renderList() {
+        const questions = getQuestionFromLocalStorage()
+
+        const html = questions.length
+            ? questions.map(toCard).join('')
+            : `<div class="mui--text-headline">You have not gave any questions</div>`
+
+        const list = document.getElementById('list')
+
+        list.innerHTML = html
+    }
+}
+
+function addToLocalStorage(data) {
+    const all = getQuestionFromLocalStorage()
+    all.push(data)
+    localStorage.setItem('questions', JSON.stringify(all))
+}
+
+function getQuestionFromLocalStorage() {
+    return JSON.parse(localStorage.getItem('questions') || '[]')
+}
+
+function toCard(question) {
+    return `
+        <div class="mui--text-black-54">
+            ${new Date(question.date).toLocaleDateString()}
+            ${new Date(question.date).toLocaleTimeString()}
+        </div>
+        <div>${question.text}</div>
+        <br>
+    `
 }
